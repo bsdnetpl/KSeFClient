4a9dac2f4ed6a88ad0d210aeeeb516ff69323f71
Krzysztof <krzysztof@engineer.com>
2025-11-18 17:34:46 +0100

    fix xml opening tag

--- KSeFClient.php
+++ KSeFClient.php
@@ -173,7 +173,7 @@ final class KSeFXAdESClient
         $certBodyB64   = $this->getCertBodyBase64();
 
         $xml = <<<XML
-<?php version="1.0" encoding="utf-8"?>
+<?xml version="1.0" encoding="utf-8"?>
 <AuthTokenRequest xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
                   xmlns:xsd="http://www.w3.org/2001/XMLSchema"
                   xmlns="http://ksef.mf.gov.pl/auth/token/2.0"
@@ -1141,4 +1141,5 @@ XML;
         ];
     }
 }
+
 
